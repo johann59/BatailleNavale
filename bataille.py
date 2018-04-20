e3fc984c7f599169dbe73412c292b1781c1aac3a
johann <johannengrand59@gmail.com>
2018-04-20 11:50:07 +0200

    Interface grille

--- bataille.py
+++ bataille.py
@@ -1,4 +1,7 @@
 import curses
+import math
+from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
+from random import randint
 
 class Navires:
     """Classe de navires"""
@@ -33,13 +36,66 @@ class Grille:
     #    print(coordX)
 
     def afficher_grille(self):
-        curses.initscr()
-        begin_x = 20; begin_y = 7
-        height = 5; width = 40
-        win = curses.newwin(height, width, begin_y, begin_x)
 
-        win.refresh()
+        curses.initscr()
+        win = curses.newwin(23, 41, 0, 0)
+        win.keypad(1)
+        win.border(0)
+        win.nodelay(1)
+
+
+        win2 = curses.newwin(23, 41, 0, 60)
+        win2.keypad(1)
+        win2.border(0)
+        win2.nodelay(1)
+
+        curses.noecho()
+        curses.curs_set(0)
+
+        key = KEY_RIGHT  
+
+        while key != 27:               # While Esc key is not pressed
+            win.border(0)              # Printing 'Score' and
+            win.addstr(0, 16, ' GENTIL ')  
+
+            #Grille
+            for y in range(0, 20, 2):
+                for x in range(0, 40, 4):
+                    if x > 0 :
+                        win.addstr(y+2, x, '|')
+                        if y < 20 and y > 0:
+                            win.addstr(y+1, x-1, '---')
+
+            #Bateau
+            for y in range(0, 20, 2):
+                for x in range(0, 40, 4):
+                        win.addch(y+2, x+2, '~')
+
+
+            win2.border(0)             
+            win2.addstr(0, 16, ' MECHANT')  
+
+            #Grille
+            for y in range(0, 20, 2):
+                for x in range(0, 40, 4):
+                    if x > 0 :
+                        win2.addstr(y+2, x, '|')
+                        if y < 20 and y > 0:
+                            win2.addstr(y+1, x-1, '---')
+
+            #Bateau
+            for y in range(0, 20, 2):
+                for x in range(0, 40, 4):
+                        win2.addch(y+2, x+2, '~')
+
+
+            prevKey = key                                                  # Previous key pressed
+            event = win.getch()
+            event2 = win2.getch()
+            key = key if event == -1 else event 
+            key = key if event2 == -1 else event2 
+
+        curses.endwin()
 
 grille = Grille('test')
 grille.afficher_grille()
-#grille.init_grille()
